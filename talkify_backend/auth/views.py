8147b073fe63575456c5b919eea793565129f3c8
Aghosh <aghosh@gmail.com>
2026-05-02 19:23:27 +0530

    feat(routing): add backend URL configs and frontend route structure

--- talkify_backend/auth/views.py
+++ talkify_backend/auth/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import *
 
-# Create your views here.
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=201)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "access": str(refresh.access_token),
+                "refresh": str(refresh)
+            })
+
+        return Response(serializer.errors, status=400)
