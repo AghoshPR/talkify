d053d4d2ccaff1a710ee8e50f47cba77ac8d83aa
Aghosh <aghosh@gmail.com>
2026-05-03 12:55:11 +0530

    feat(slice):authentications slice and store are configered

--- talkify_frontend/src/App.jsx
+++ talkify_frontend/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- talkify_frontend/src/Components/Services/Api.jsx
+++ talkify_frontend/src/Components/Services/Api.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-const Api = () => {
-  return (
-    <div>Api</div>
-  )
-}
+const Api = () =>
+  axios.create({
+    baseURL: "http://127.0.0.1:8000/api/",
+  });
 
-export default Api
+
+  Api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token")
+
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  })
+
+export default Api;
