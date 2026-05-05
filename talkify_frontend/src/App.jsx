048d51f9675b1e57afce5462519fef4a92131f6d
Aghosh <aghosh@gmail.com>
2026-05-05 13:50:47 +0530

    add default route redirect to login

--- talkify_frontend/src/App.jsx
+++ talkify_frontend/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
