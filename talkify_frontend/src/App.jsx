26e8a75714ad88ac0d3cc160080f52625446581e
Aghosh <aghosh@gmail.com>
2026-05-05 13:52:42 +0530

    fix:navigate import added

--- talkify_frontend/src/App.jsx
+++ talkify_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import ProtectedRoute from "./Components/ProtectedRoute";
 import Login from "./Components/Pages/Login";
