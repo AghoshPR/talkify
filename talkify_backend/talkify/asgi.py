7cbff3d97ffb3bd3316ab4f689f80fad405455ac
Aghosh <aghosh@gmail.com>
2026-05-08 00:45:31 +0530

    fix:asgi settings order

--- talkify_backend/talkify/asgi.py
+++ talkify_backend/talkify/asgi.py
@@ -1,18 +1,31 @@
-
-
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter,URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkify.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'talkify.settings'
+)
+
+from django.core.asgi import get_asgi_application
+
+from channels.routing import (
+    ProtocolTypeRouter,
+    URLRouter,
+)
+
+from channels.auth import (
+    AuthMiddlewareStack,
+)
+
+import chat.routing
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http":django_asgi_app,
-    "websocket":AuthMiddlewareStack(
+
+    "http": django_asgi_app,
+
+    "websocket": AuthMiddlewareStack(
+
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
