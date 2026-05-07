014af954224ffec6372fd011f30f827d9e2eb25b
Aghosh <aghosh@gmail.com>
2026-05-08 01:02:02 +0530

    added production custom domain settings

--- talkify_backend/talkify/settings.py
+++ talkify_backend/talkify/settings.py
@@ -160,10 +160,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://talkify-kohl.vercel.app",
+    "https://talkify.aghosh.site",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://talkify-kohl.vercel.app",
+    "https://talkify.aghosh.site",
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
